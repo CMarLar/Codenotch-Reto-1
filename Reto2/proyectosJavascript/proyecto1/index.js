7bb9bd056fbd8982715b8f01e03043a3454555db
Carlos Marina <c.marina.larrosa@gmail.com>
2022-10-18 13:28:42 +0200

    Primer commit

---
+++ Reto2/proyectosJavascript/proyecto1/index.js
@@ -0,0 +1,7 @@
+const suma = (op1,op2) => op1 + op2;
+const resta = (op1,op2) => op1 - op2;
+const mult = (op1,op2) => op1 * op2;
+const div = (op1,op2) => op1 / op2;
+const cuadrado = (op1) => op1 * op1;
+
+module.exports = {suma,resta,mult,div,cuadrado};
---
+++ Reto2/proyectosJavascript/proyecto1/run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index");
+
+console.log(myLib.suma(4,2));//Tienes que buscar la función como si fuera una propiedad dentro del objeto myLib (la librería)
+console.log(myLib.resta(4,2));
+console.log(myLib.mult(4,2));
+console.log(myLib.div(4,2));
+console.log(myLib.cuadrado(4));
